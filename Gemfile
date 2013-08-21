93f0e8a3cae780307339731b2cf823f1c130cb81
Andre D <adettler@gmail.com>
2013-08-21 20:27:23 +0800

    intial boostrap gem install

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ end
 gem 'sass-rails', '~> 4.0.0.rc2'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
-gem 'bootstrap-sass', '~> 2.2.2.0'
+gem 'bootstrap-sass', '~> 2.3.2.1'
 gem 'rails_12factor', group: :production
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,7 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require_tree .
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -10,7 +14,5 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+
