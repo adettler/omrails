403c9a57d865fab63f2ced287e3fb2ebf95b02a5
Andre D <adettler@gmail.com>
2013-08-22 19:32:47 +0800

    added bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,3 @@
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require_tree .
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -14,5 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require_tree .
 
